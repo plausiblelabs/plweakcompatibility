a6a1f2725b16ca7ce1f63c52926a086053de4ee2
landonf <landonf@0b19fab4-3b79-e111-ab8d-001cc008e099>
2012-03-30 20:07:26 +0000

    Add support for testing on iOS devices.
    git-svn-id: https://src.office.plausiblelabs.com/repo/PLWeakCompatibility/trunk@34 0b19fab4-3b79-e111-ab8d-001cc008e099

---
+++ PLWeakCompatibility-iOS-TestRunner/PLAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  PLAppDelegate.h
+//  PLWeakCompatibility-iOS-TestRunner
+//
+//  Created by Landon Fuller on 3/30/12.
+//  Copyright (c) 2012 Plausible Labs Cooperative, Inc. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface PLAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ PLWeakCompatibility-iOS-TestRunner/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  PLWeakCompatibility-iOS-TestRunner
+//
+//  Created by Landon Fuller on 3/30/12.
+//  Copyright (c) 2012 Plausible Labs Cooperative, Inc. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "PLAppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([PLAppDelegate class]));
+    }
+}
