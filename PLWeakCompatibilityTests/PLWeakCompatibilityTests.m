b7f690b02d9b91f4573805e8eeecabef578613fa
Mike Ash <mike@mikeash.com>
2012-05-10 11:35:26 -0400

    Add tests for deadlock in dealloc and acquiring a ZWR during release. The dealloc test does not yet pass, and is written in preparation for code that will allow dealloc to run without the ZWR lock held. The release test does currently pass, and exists to make sure that this scenario continues to work even after the dealloc code is changed.

--- PLWeakCompatibilityTests/PLWeakCompatibilityTests.m
+++ PLWeakCompatibilityTests/PLWeakCompatibilityTests.m
@@ -16,21 +16,30 @@
 
 
 #define TESTCLASS(name, superclass) \
-    @interface name : superclass @end \
+    @interface name : superclass \
+    @property (copy) dispatch_block_t releaseBlock; \
+    @property (copy) dispatch_block_t deallocBlock; \
+    @end \
     @implementation name { SEL _release; } \
+    @synthesize releaseBlock = _releaseBlock ## name, deallocBlock = _deallocBlock ## name; \
     - (id) init { \
         self = [super init]; \
         _release = sel_getUid("release"); \
-        Method m = class_getInstanceMethod([name class], _release); \
+        Method m = class_getInstanceMethod([name class], @selector(release_toswizzle)); \
         class_addMethod([name class], _release, method_getImplementation(m), method_getTypeEncoding(m)); \
         return self; \
     } \
     - (void) release_toswizzle { \
+        if ([self releaseBlock] != nil) [self releaseBlock](); \
+        if (_release == NULL) _release = sel_getUid("release"); \
         struct { void *obj, *class; } superStruct = { (__bridge void *)self, (__bridge void *)[superclass class] }; \
         void (*msgSendSuper)(void *, SEL) = (void (*)(void *, SEL))objc_msgSendSuper; \
         msgSendSuper(&superStruct, _release); \
     } \
-    - (void) dealloc { _release = NULL; } \
+    - (void) dealloc { \
+        if ([self deallocBlock] != nil) [self deallocBlock](); \
+        _release = NULL; \
+    } \
     @end
 
 TESTCLASS(PLWeakCompatibilityTestClass1, NSObject)
@@ -97,6 +106,99 @@ TESTCLASS(PLWeakCompatibilityTestClass3, PLWeakCompatibilityTestClass2)
 
         STAssertNil(weakObj1, @"Test object 1 should be nil");
         STAssertNil(weakObj2, @"Test object 2 should be nil");
+    }];
+}
+
+- (void) testDeallocDeadlock {
+    [self enumerateConfigurations: ^{
+        @autoreleasepool {
+            __weak id weakSelf = self;
+            
+            PLWeakCompatibilityTestClass1 *obj = [[PLWeakCompatibilityTestClass1 alloc] init];
+            [obj setDeallocBlock: ^{
+                dispatch_group_t group = dispatch_group_create();
+                dispatch_group_async(group, dispatch_get_global_queue(0, 0), ^{
+                    [weakSelf self];
+                });
+                dispatch_group_wait(group, DISPATCH_TIME_FOREVER);
+                dispatch_release(group);
+            }];
+            __weak id weakObj = obj;
+            obj = nil;
+            weakObj = nil;
+        }
+    }];
+}
+
+- (void) testGrabDuringRelease {
+    [self enumerateConfigurations: ^{
+        /* Declare the target and a weak reference to it. Target is a void * so we can have
+         * better control over its lifetime. Also note the massive proliferation of
+         * @autoreleasepool blocks for the same reason. */
+        __block const void *target;
+        __weak PLWeakCompatibilityTestClass1 *weakTarget;
+        
+        @autoreleasepool {
+            target = CFBridgingRetain([[PLWeakCompatibilityTestClass1 alloc] init]);
+            weakTarget = (__bridge id)target;
+        }
+        
+        /* Set up the block that will be used to acquire the weak reference. Do it now so
+         * all of the weak calls used to set up the block are complete before we start blocking. */
+        __block BOOL acquired;
+        dispatch_semaphore_t acquiredSem = dispatch_semaphore_create(0);
+        dispatch_block_t acquireBlock;
+        @autoreleasepool {
+            acquireBlock = [^{
+                id target = weakTarget;
+                acquired = target != nil;
+                dispatch_semaphore_signal(acquiredSem);
+            } copy];
+        }
+
+        /* Make the object block when released. */
+        dispatch_semaphore_t releaseStartedSem = dispatch_semaphore_create(0);
+        dispatch_semaphore_t releaseSem = dispatch_semaphore_create(0);
+        void (*setReleaseBlock)(const void *, SEL, dispatch_block_t);
+        @autoreleasepool {
+            setReleaseBlock = (void *)[(__bridge id)target methodForSelector: @selector(setReleaseBlock:)];
+        }
+        /* When this line runs, target MUST have a retain count of 1, and must do nothing more than be released
+         * and deallocate, because this release block is only safe to run once. The rest of the code is set up
+         * to make that happen. */
+        setReleaseBlock(target, @selector(setReleaseBlock:), ^{
+            dispatch_semaphore_signal(releaseStartedSem);
+            dispatch_semaphore_wait(releaseSem, DISPATCH_TIME_FOREVER);
+            dispatch_release(releaseSem);
+        });
+        
+        /* Start the release in the background, but it will wait. */
+        dispatch_async(dispatch_get_global_queue(0, 0), ^{
+            CFRelease(target);
+            target = NULL;
+        });
+        
+        /* We want to wait for it to get started here. */
+        dispatch_semaphore_wait(releaseStartedSem, DISPATCH_TIME_FOREVER);
+        
+        /* target is now blocked in release. Start trying to acquire it. */
+        dispatch_async(dispatch_get_global_queue(0, 0), acquireBlock);
+        
+        /* Wait a moment to ensure everybody is blocked. */
+        [NSThread sleepUntilDate: [NSDate dateWithTimeIntervalSinceNow: 1.0]];
+        
+        /* Unblock the release. */
+        dispatch_semaphore_signal(releaseSem);
+        
+        /* Wait for acquisition. */
+        dispatch_semaphore_wait(acquiredSem, DISPATCH_TIME_FOREVER);
+        
+        /* Make sure we got nil. */
+        STAssertFalse(acquired, @"Should have loaded nil from the weak reference while releasing.");
+        
+        /* Clean up. */
+        dispatch_release(releaseStartedSem);
+        dispatch_release(acquiredSem);
     }];
 }
 
